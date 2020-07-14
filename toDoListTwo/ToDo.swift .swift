3f7b4eb8c6a9a52c36d45cb9c3c7ef601c17479e
Zoe Howes <zoepascalehowes@gmail.com>
2020-07-14 13:35:59 -0700

    /Users/zoehowes/Desktop/toDoListTwo

---
+++ toDoListTwo/ToDo.swift .swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift .swift
+//  toDoListTwo
+//
+//  Created by Zoe Howes on 7/14/20.
+//  Copyright © 2020 Zoe Howes. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+var name = ""
+    var important = false
+
+}
